89a962d9fada5e68e8b3fbb6239bb6b0eb8de21d
lammymy <my.lvk27@gmail.com>
2024-03-19 13:05:56 +0700

    model product

---
+++ models/analytics/product.sql
@@ -0,0 +1,9 @@
+SELECT 
+  product.stock_item_id AS product_id
+  , product.stock_item_name AS product_name
+  , COALESCE(product.brand, 'Undefined') AS brand_name
+  , product.supplier_id
+  , supplier.supplier_name
+ FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`  AS product
+ JOIN `learn-dbt-417411.learn_dbt.supplier` AS supplier
+  ON product.supplier_id = supplier.supplier_id
